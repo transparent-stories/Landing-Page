91f135b966e254985eea62b45f6208a2b2ed4898
transparent-stories <transparentstoriess@gmail.com>
2026-07-01 21:02:15 +0530

    any type issue

--- app/components/section/MarketingPlanSection.tsx
+++ app/components/section/MarketingPlanSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Image from 'next/image'
+import Image, {StaticImageData } from 'next/image'
 
 import { MarketingPlanCards } from '../ui/MarketingPlanCard'
 
@@ -26,7 +26,7 @@ interface MarketingPlanSectionProps {
     ctaText: string
     ctaLink: string
 
-    mainImage: any
+    mainImage: string | StaticImageData
 
     cards: MarketingPlanCard[]
   }
